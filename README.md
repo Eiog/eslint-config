886b69d57fe3b952df66b0bd5e01731736fc7dcf
antrioe <antrioe@gmali.com>
2024-04-30 14:58:20 +0800

    feat(components): feat eiog

--- README.md
+++ README.md
@@ -1,20 +1,10 @@
-# library-starter
+My eslint config based on [@antfu/eslint-config](https://github.com/antfu/eslint-config)
 
-Features:
+---
 
-- Bundle with [tsup](https://github.com/egoist/tsup)
-- Test with [vitest](https://vitest.dev)
+- Enable UnoCss
+- Enable Formatters
 
-# Usage
-### 本地包测试
-当前目录下执行：
-```bash
-pnpm link --global --dir=./
-```
-目标目录：
-```bash
-pnpm link --global <package name>
-```
 ## License
 
 MIT
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
-export const one = 1
-export const two = 2
+import type { Awaitable, OptionsConfig, TypedFlatConfigItem } from '@antfu/eslint-config'
+import type { Linter } from 'eslint'
+import type { FlatConfigComposer } from 'eslint-flat-config-utils'
+import antfu from '@antfu/eslint-config'
+
+export function eiog(options?: OptionsConfig & TypedFlatConfigItem, ...userConfigs: Awaitable<TypedFlatConfigItem | TypedFlatConfigItem[] | FlatConfigComposer<any, any> | Linter.FlatConfig[]>[]): ReturnType<typeof antfu> {
+  return antfu({
+    unocss: true,
+    formatters: true,
+    ...options,
+  }, ...userConfigs)
+}
